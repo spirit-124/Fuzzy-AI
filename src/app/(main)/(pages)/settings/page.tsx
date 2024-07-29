878e03c30df5d59f6b59f150beb3eb180e5495b6
spirit-124 <bajrangofficial580@gmail.com>
2024-07-29 19:26:49 +0530

    [Setup] Prisma ORM

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -1,9 +1,25 @@
+import ProfileForm from "@/components/forms/profile-form";
 import React from "react";
 
 type Props = {};
-
+// TODO: Wire Up profile picture
 const SettingsPage = (props: Props) => {
-  return <div>SettingsPage</div>;
+  return (
+    <div className=" flex flex-col gap-4">
+      <h1 className=" text-4xl sticky top-0 z-[10] flex items-center justify-between  border-b bg-background/50 p-6 backdrop-blur-lg">
+        <span>SettingPage</span>
+      </h1>
+      <div className=" flex flex-col gap-10 p-6">
+        <div>
+          <h2 className="  text-2xl font-bold"> User Profile</h2>
+          <p className=" text-base text-white/50">
+            Add or update your information{" "}
+          </p>
+        </div>
+        <ProfileForm />
+      </div>
+    </div>
+  );
 };
 
 export default SettingsPage;
---
+++ src/components/forms/profile-form.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { EditUserProfileSchema } from "@/lib/types";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Loader2 } from "lucide-react";
+
+type Props = {
+  user: any;
+  onUpdate?: any;
+};
+
+const ProfileForm = (props: Props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const form = useForm<z.infer<typeof EditUserProfileSchema>>({
+    mode: "onChange",
+    resolver: zodResolver(EditUserProfileSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+    },
+  });
+  return (
+    <Form {...form}>
+      <form onSubmit={() => {}} className=" flex flex-col gap-6">
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">User full name</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder="Name" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          disabled={true}
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">User Email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className=" self-start  hover:text-white hover:bg-[#2F006B]  "
+        >
+          {isLoading ? (
+            <>
+              <Loader2 className=" mr-2 h-4 w-4 animate-spin" />
+              Saving
+            </>
+          ) : (
+            "Save user settings"
+          )}
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default ProfileForm;
